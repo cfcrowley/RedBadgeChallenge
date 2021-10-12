0d95e42cbb125f3cda066495c7f7cc7a2408477a
Chris Crowley <cfcrowely42@gmail.com>
2021-10-12 16:28:36 -0400

    added Legacy data

---
+++ MagicCreator.MVC/MagicCreator.Data/Legacy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicCreator.Data
+{
+    public class Legacy
+    {
+        [Key]
+        public int LegacyId { get; set; }
+        [Required]
+        [Display(Name = "Deck Name")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Card Count for Deck")]
+        public int CardCount { get; set; }
+        public string DeckStyle { get; set; }
+        public virtual List<Card> Cards { get; set; }
+
+    }
+}
