027e10bec4b06268bfff18252129086e1844f514
Chris Crowley <cfcrowely42@gmail.com>
2021-10-12 15:45:13 -0400

    initial commit

---
+++ MagicCreator.MVC/MagicCreator.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MagicCreator.MVC.Startup))]
+namespace MagicCreator.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
