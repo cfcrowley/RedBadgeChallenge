8c9da51a0d8ef37a0f77eb7d482eaf604d78dc45
Chris Crowley <cfcrowely42@gmail.com>
2021-10-12 16:50:14 -0400

    finished data models

---
+++ MagicCreator.MVC/MagicCreator.Data/Game.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicCreator.Data
+{
+    public class Game
+    {
+        [Key]
+        public int GameId { get; set; }
+        [Required]
+        [Display(Name ="Your Deck ID")]
+        public int DeckId { get; set; }
+        [Required]
+        [Display(Name ="Enemy Deck ID")]
+        public int EnemyID { get; set; }
+        public string Result { get; set; }
+        public virtual 
+
+    }
+}
