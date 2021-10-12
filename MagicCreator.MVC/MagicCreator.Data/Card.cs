c370a65ff4644a22b72e40fa811f3dedd18751e3
Chris Crowley <cfcrowely42@gmail.com>
2021-10-12 16:16:19 -0400

    added card model

---
+++ MagicCreator.MVC/MagicCreator.Data/Card.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicCreator.Data
+{
+    public class Card
+    {
+        [Key]
+        public int CardId { get; set; }
+        [Required]
+        [Display(Name ="Card Name")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name ="Card Type")]
+        public string Type { get; set; }
+        [Required]
+        [Display(Name ="Mana Value")]
+        public int ManaValue { get; set; }
+    }
+}
