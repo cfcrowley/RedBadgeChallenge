ab5048a22d314fa6aa3f9507e6aa9c725f97c151
Chris Crowley <cfcrowely42@gmail.com>
2021-10-12 15:53:14 -0400

    initial setup

--- MagicCreator.MVC/MagicCreator.MVC/App_Start/IdentityConfig.cs
+++ MagicCreator.MVC/MagicCreator.MVC/App_Start/IdentityConfig.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
 using MagicCreator.MVC.Models;
+using MagicCreator.Data;
 
 namespace MagicCreator.MVC
 {
