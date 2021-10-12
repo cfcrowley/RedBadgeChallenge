9cbc3a44bebbec70df1281ddd2774601cc59b7c0
Chris Crowley <cfcrowely42@gmail.com>
2021-10-12 16:27:06 -0400

    added Modern data

---
+++ MagicCreator.MVC/MagicCreator.Data/Modern.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicCreator.Data
+{
+    public class Modern
+    {
+        [Key]
+        public int ModernId { get; set; }
+        [Required]
+        [Display(Name ="Deck Name")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name ="Card Count for Deck")]
+        public int CardCount { get; set; }
+        public string DeckStyle { get; set; }
+        public virtual List<Card> Cards { get; set; }
+
+    }
+}
