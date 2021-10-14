cfdccf5bf167343953bbcf35ae734dfae8b041c4
Chris Crowley <cfcrowely42@gmail.com>
2021-10-14 11:58:18 -0400

    added Legacy Create

---
+++ MagicCreator.MVC/MagicCreator.Models/Legacy/LegacyCreate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicCreator.Models.Legacy
+{
+    public class LegacyCreate
+    {
+        public string Name { get; set; }
+        public int CardCount { get; set; }
+        public string DeckStyle { get; set; }
+    }
+}
---
+++ MagicCreator.MVC/MagicCreator.Models/Modern/ModernDetail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicCreator.Models.Modern
+{
+    public class ModernDetail
+    {
+        public int ModernId { get; set; }
+        public string Name { get; set; }
+        public int CardCount { get; set; }
+        public string DeckStyle { get; set; }
+        public double AvgRating { get; set; }
+    }
+}
