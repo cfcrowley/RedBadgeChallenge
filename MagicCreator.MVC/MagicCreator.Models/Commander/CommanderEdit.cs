50efb74eca4a6bbfd0fb8370bb359fb60c0ecda1
Chris Crowley <cfcrowely42@gmail.com>
2021-10-14 12:15:54 -0400

    added Commander Models

---
+++ MagicCreator.MVC/MagicCreator.Models/Commander/CommanderEdit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicCreator.Models.Commander
+{
+    public class CommanderEdit
+    {
+        public int CommanderId { get; set; }
+        public string Name { get; set; }
+        public int CardCount { get; set; }
+        public string General { get; set; }
+        public string DeckStyle { get; set; }
+    }
+}
