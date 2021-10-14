35192f515712f16e25c0bfcc64752ee084903da9
Chris Crowley <cfcrowely42@gmail.com>
2021-10-14 11:44:13 -0400

    created modern Create

---
+++ MagicCreator.MVC/MagicCreator.Models/Modern/ModernCreate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicCreator.Models.Modern
+{
+    public class ModernCreate
+    {
+        public string Name { get; set; }
+        public int CardCount { get; set; }
+        public string DeckStyle { get; set; }
+      
+    }
+}
