b281a60dc21ceaa8ca5842832ba55fec476da1a5
Chris Crowley <cfcrowely42@gmail.com>
2021-10-14 12:01:37 -0400

    finished Legacy Models

---
+++ MagicCreator.MVC/MagicCreator.Models/Legacy/LegacyDetail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicCreator.Models.Legacy
+{
+    public class LegacyDetail
+    {
+        public int LegacyId { get; set; }
+        public string Name { get; set; }
+        public int CardCount { get; set; }
+        public string DeckStyle { get; set; }
+        public double AvgRating { get; set; }
+    }
+}
