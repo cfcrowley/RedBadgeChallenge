c2c1abc28e262a74cde240837cc93c7e27806d0d
Chris Crowley <cfcrowely42@gmail.com>
2021-10-14 09:27:33 -0400

    added Card detail model

---
+++ MagicCreator.MVC/MagicCreator.Models/CardDetail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicCreator.Models
+{
+    public class CardDetail
+    {
+        public int CardId { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int ManaValue { get; set; }
+    }
+}
